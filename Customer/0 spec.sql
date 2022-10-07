a0417129eb19c8ea9f7fb89c4b77e5fb103322f4
chayaestherlangsner <celangsner@gmail.com>
2022-10-07 02:14:04 -0400

    needs corrections

--- Customer/0 spec.sql
+++ Customer/0 spec.sql
@@ -1,16 +1,26 @@
 /*
 Customer
+--cel missing CustomerID column
+--cel diff varchar allowance in table
     ShopName varchar(30) not null not blank
     FirstName varchar(30) not null not blank
     LastName varchar(30) not null not blank
+--cel diff varchar allowance in table
     StreetAddress varchar(30) not null not blank
     City varchar(30) not null not blank
+--cel diff varchar allowance in table
+--cel didn't write the constraint
     StateCode varchar(20) not null not blank
     Zip varchar(10) not null must be between 5 and 10 digits
     PhoneNumber varchar(15) must be between 10 and 15 digits
+--cel diff constraint in table 
     YearCreated int not null greater than zero
+--cel diff constraint in table 
     AmoutPurchased2021 int not null greater than zero
+--cel diff constraint in table 
     AmoutPurchased2022 int not null greater than zero
+--cel diff in table 
     Terms varchar(100) not null 
     Total Paid int not null must be greater than zero
+--cel you didnt write the persisted columns and multi column constraints
 */
--- Customer/2  tables.sql
+++ Customer/2  tables.sql
@@ -10,13 +10,18 @@ create table dbo.Customer(
     StreetAddress varchar(50) not null constraint ck_Customer_StreetAddress_cannot_be_blank check(StreetAddress <> ''),
     City varchar(30) not null constraint ck_Customer_City_cannot_be_blank check(City <> ''),
     StateCode char(2) not null constraint ck_Customer_StateCode_cannot_be_either_NJ_NY_MD_or_PA check(StateCode in ('NJ', 'NY', 'MD', 'PA')),
+--cel why varchar of 10 if only delivering to NY, NY, MD, PA 
     Zip varchar(10) not null constraint ck_Customer_Zip_must_be_between_5_and_10_digits check(len(Zip) between 5 and 10),
     PhoneNumber varchar(15) not null constraint ck_Customer_PhoneNumber_must_be_between_10_and_15_digits check(len(PhoneNumber) between 10 and 15),
     YearCreated int not null constraint ck_Customer_YearCreated_must_be_greater_than_or_equal_to_1980 check(YearCreated >= 1980),
     AmountPurchased2021 int not null constraint ck_Customer_AmountPurchased2021_must_be_equal_to_or_greater_than_200 check(AmountPurchased2021 >= 200),
+--2022 is not over yet
     AmountPurchased2022 int not null constraint ck_Customer_AmountPurchased2022_must_be_equal_or_greater_than_200 check(AmountPurchased2022 >= 200),
+--cel why is the varchar 200 if 'COD', 'Net 15', 'Net 30'and 'Net 60' are the only options
+--cel why are blanks allowed
     Terms varchar(200) not null default '' constraint ck_Customer_terms_must_be_COD_Net15_Net30_Net60_or_blank check(Terms in ('COD', 'Net 15', 'Net 30', 'Net 60', '')),
     TotalPaid int not null constraint ck_Customer_TotalPaid_must_be_greater_than_zero check(TotalPaid > 0),
+--cel CustomerCode needs to be unique itself
     CustomerCode as concat(substring(ShopName, 1, 3), substring(FirstName, 1, 3), right(PhoneNumber, 4)),
     constraint u_Customer_ShopName_CustomerCode unique(ShopName, CustomerCode)
 )
